d5dca769caf76e1b805ad8f12f21ca1488bd920f
zzy0371 <496575233@qq.com>
2020-02-17 11:10:22 +0800

    投票小程序  完成

--- end/demo1/bookdemo/polls/views.py
+++ end/demo1/bookdemo/polls/views.py
@@ -1,13 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("首页")
+    questions = Question.objects.all()
+    return render(request,'polls/index.html',{"questions":questions})
+    # return HttpResponse("首页")
 
 def detail(request,qid):
-    return HttpResponse("详情页"+qid)
+    print(qid,"+++")
+    if request.method == "GET":
+        try:
+            question = Question.objects.get(id=qid)
+            print(question, "--")
+            return render(request, 'polls/detail.html', {"question": question})
+
+        except Exception as e:
+            print(e)
+            return HttpResponse("问题不合法")
+    elif request.method == "POST":
+        choiceid = request.POST.get("num")
+        try:
+            choice = Choices.objects.get(id=choiceid)
+            choice.votes+=1
+            choice.save()
+            # 返回当前投票问题的投票结果页
+            url = reverse("polls:result",args=(qid,))
+            # 投票成功 返回投票结果
+            return redirect(to=url)
+
+        except:
+            return HttpResponse("选项不合法")
+
+
+
+    # return HttpResponse("详情页"+qid)
 
 def result(request,qid):
-    return HttpResponse("结果也"+qid)
+
+    try:
+        question = Question.objects.get(id=qid)
+        return render(request,'polls/result.html',{"question":question})
+    except Exception as e:
+        print(e)
+        return HttpResponse("问题不合法")
