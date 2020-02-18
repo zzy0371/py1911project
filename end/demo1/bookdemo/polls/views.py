d53ed4aef0b90faac9446ee2e7f6e3d1b4ca15dd
zzy0371 <496575233@qq.com>
2020-02-18 17:26:47 +0800

    投票路由守卫

--- end/demo1/bookdemo/polls/views.py
+++ end/demo1/bookdemo/polls/views.py
@@ -41,21 +41,44 @@ class IndexView(ListView):
 def detail(request, qid):
     print(qid, "+++")
     if request.method == "GET":
-        try:
-            question = Question.objects.get(id=qid)
-            print(question, "--")
-            # 使用render发起一起请求
-            return render(request, 'polls/detail.html', {"question": question})
+        print("当前用户",request.user.username)
+        if request.user and request.user.username != "":
+            # 已经登录过了
+            # print("++",request.user.questions,type(request.user.questions.all()))
+            try:
+                question = Question.objects.get(id=qid)
+                if question in request.user.questions.all():
+                    print("已经投过票")
+                    url = reverse("polls:result",args=(qid))
+                    return redirect(to=url)
+                else:
+                    try:
+                        # question = Question.objects.get(id=qid)
+                        print(question, "--")
+                        # 使用render发起一起请求
+                        return render(request, 'polls/detail.html', {"question": question})
 
-        except Exception as e:
-            print(e)
-            return HttpResponse("问题不合法")
+                    except Exception as e:
+                        print(e, "--")
+                        return HttpResponse("问题不合法")
+
+            except Exception as e:
+                print(e,"-----")
+
+
+
+
+        else:
+            url = reverse("polls:login")
+            return redirect(to=url)
     elif request.method == "POST":
         choiceid = request.POST.get("num")
         try:
             choice = Choices.objects.get(id=choiceid)
             choice.votes+=1
             choice.save()
+
+            request.user.questions.add( Question.objects.get(id=qid) )
             # 返回当前投票问题的投票结果页
             url = reverse("polls:result", args=(qid,))
             # 投票成功 返回投票结果
