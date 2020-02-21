3e7309b74c0061c281ef8525bda9e28bb0bc6a4c
zzy0371 <496575233@qq.com>
2020-02-21 09:52:47 +0800

    自定义标签 过滤器

---
+++ end/demo2/blog/apps/blogapp/templatetags/myfun.py
@@ -0,0 +1,16 @@
+from django.template import Library
+from ..models import Article
+register = Library()
+
+@register.filter
+def dateFormat(data):
+    return "%d-%d-%d"%(data.year,data.month,data.day)
+
+@register.filter
+def authorFormat(author,info):
+    return info+":"+author.upper()
+
+@register.simple_tag
+def get_latestarticles(num=3):
+    return Article.objects.all().order_by("-create_time")[:num]
+
